d8b2135757ad711f348f0c6c7979ddb19743fde0
Bhoomi Tiwari <bhoomitiwari56@gmail.com>
2026-04-13 04:54:35 +0530

    Update Billboard.cs

--- ARMobile/Assets/Scripts/Billboard.cs
+++ ARMobile/Assets/Scripts/Billboard.cs
@@ -4,7 +4,6 @@ public class Billboard : MonoBehaviour {
     void Update() {
         if (Camera.main != null) {
             transform.LookAt(Camera.main.transform);
-            // Keep him upright so he doesn't tilt forward
             transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
         }
     }
